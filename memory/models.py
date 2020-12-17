fecbfaffc8c381068de353adc9bd25a6c1cf8fed
paul-leonard <paul.ryan.leonard@gmail.com>
2020-12-17 09:01:50 -0800

    created story model and templates

--- memory/models.py
+++ memory/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+class Story(models.Model):
+  title = models.CharField(max_length=255)
+  body = models.TextField(default="")
+  author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+
+  def __str__(self):
+    return self.title
